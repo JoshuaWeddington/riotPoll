eea285ff57a80607c16ea786bcd6283dab7c91fa
BigSquecker <84789957+BigSquecker@users.noreply.github.com>
2022-02-10 13:24:14 -0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 09:24:07 2022
+
+@author: msustudent
+"""
+
+
+key = "-- Insert Key Here --"
+queryChallengers = True
+queryGrandmasters = False
+queryMasters = False
+queryPlatDiamond = False
---
+++ hextechAugmentedNetwork.py
@@ -0,0 +1,5 @@
+import keras
+import riotPoll
+
+dataset = riotPoll.matchInfo
+
